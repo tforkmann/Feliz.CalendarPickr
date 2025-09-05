4e36d4dd4d4690cccc8c4ecfbf937c3ce9c50873
Tim Forkmann <tim.forkmann@msu-solutions.de>
2025-09-05 09:39:12 -0400

    Bumping version to 0.1.4

--- Build.fs
+++ Build.fs
@@ -173,7 +173,7 @@ let isNullOrWhiteSpace = String.IsNullOrWhiteSpace
 
 // Workaround for https://github.com/fsharp/FAKE/issues/2242
 let pushPackage _ =
-    let nugetCmd fileName key = sprintf "nuget push %s -k %s -s nuget.org" fileName key
+    let nugetCmd fileName key = sprintf "nuget push %s -k %s -s https://api.nuget.org/v3/index.json" fileName key
     let key =
         //Environment.environVarOrFail "nugetKey"
         match getBuildParam "nugetkey" with
